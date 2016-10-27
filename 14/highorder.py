18acfac34818fce262b087b5f9c056376f0c9f1f
Jaehak Song <master@hpi.cc>
2016-10-28 00:50:20 +0900

    Create highorder.py

---
+++ 14/highorder.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        return reduce(lambda x, y: self.prefix(x, y), strs)
+    
+    def prefix(self, left, right):
+        minLen = min(map(len, [left, right]))
+        for i in xrange(0, minLen):
+            if left[i] != right[i]:
+                return left[:i]
+        return left[:minLen]
